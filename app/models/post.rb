e2bf71e9766fbb5b759422d8ae994654ae306a9f
bnb <deathproooof@gmail.com>
2023-06-29 23:38:08 +0900

    修正:post-indexをタイル状に表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 
     has_many_attached :images do |attachable|
-        attachable.variant :thumb, resize_to_limit: [300, 300]
+        attachable.variant :thumb, resize_to_fill: [150, 150]
     end
 
     validates :images, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']

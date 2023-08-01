562ff78eae178223858de2cfdb473debd2dafef6
bnb <deathproooof@gmail.com>
2023-08-01 18:47:20 +0900

    修正:マイグレーション(ポストグレ用)

--- db/migrate/20230801071544_change_user_id_to_integer.rb
+++ db/migrate/20230801071544_change_user_id_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeUserIdToInteger < ActiveRecord::Migration[7.0]
   def change
-    change_column :tags, :user_id, :integer
+    change_column :tags, :user_id, :integer, using: 'user_id::integer'
   end
 end

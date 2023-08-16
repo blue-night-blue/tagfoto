7b688b313f11f25e5e67ca2fc3efdb0cb4cbf320
bnb <deathproooof@gmail.com>
2023-08-16 12:33:57 +0900

    追加:CircleCI

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
-    has_secure_password
     before_save { self.email = email.downcase }
     before_save { self.name = name.downcase }
-
-    validates :password, length: { maximum: 255 }, length: { minimum: 6 }
 
     VALID_NAME_REGEX = /\A[a-z\d\-\_]+\z/i
     validates :name,presence:true,uniqueness: true, length: { maximum: 20 }, 
@@ -13,5 +10,8 @@ class User < ApplicationRecord
     validates :email,presence:true,length: { maximum: 255 },
                      format: { with: VALID_EMAIL_REGEX },
                      uniqueness: true
+
+    has_secure_password
+    validates :password, length: { maximum: 255 }, length: { minimum: 6 }
 
 end

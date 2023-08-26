a5e9a1c4c5dd2ad48dd7aaead017841e37607dfc
bnb <deathproooof@gmail.com>
2023-08-26 11:34:29 +0900

    追加:初seed(今更？)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+user_count = 3
+
+users = []
+user_count.times do |i|
+    user = User.find_or_create_by(name: "sample#{i+1}",email: "sample#{i+1}@example.com")
+    user.password = "sample#{i+1}"
+    user.save
+    users << user
+end
+
+user_count.times do |i|
+  secret_phrase = SecretPhrase.find_or_create_by(user_id: users[i].id)
+  secret_phrase.password = "secret#{i+1}"
+  secret_phrase.save
+
+  ApprovedUser.find_or_create_by(
+    user_id: users[i].id,
+    approved_user_id: users[i-1].id
+  )
+
+  ApprovedUser.find_or_create_by(
+    user_id: users[i].id,
+    approved_user_id: users[i-2].id
+  )
+end
+
+puts "#{user_count} posts have been created!"
